bbdfad3f2c8c0a7c9ea08f546e35155fe4b2b77c
Adarsh Agrawal <adarshagrawal105@gmail.com>
2025-10-13 12:44:18 -0700

    step 1 (untested)

--- llrec.cpp
+++ llrec.cpp
@@ -5,3 +5,24 @@
 //*********************************************
 
 
+void llpivot(Node *&head, Node *&smaller, Node *&larger, int pivot){
+    if (head == nullptr){ // receiving an empty list
+        smaller = nullptr; 
+        larger = nullptr; 
+        return; 
+    }
+
+    Node* headTemp = head; 
+    head = head->next; 
+
+    if (headTemp->val > pivot){
+        llpivot(head, smaller, larger, pivot);
+        headTemp->next = larger; 
+        larger = headTemp; 
+    }
+    else {
+        llpivot(head, smaller, larger, pivot);
+        headTemp->next = smaller;
+        smaller = headTemp;
+    }
+}
