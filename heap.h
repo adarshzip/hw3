481386ac2dc54602c61a19cb5fc08a399e011a33
Adarsh Agrawal <adarshagrawal105@gmail.com>
2025-10-14 20:13:20 -0700

    pop and onwards unfinished in heap

--- heap.h
+++ heap.h
@@ -61,14 +61,34 @@ public:
 
 private:
   /// Add whatever helper functions and data members you need below
-
-
-
-
+  int m_; 
+  PComparator comp_;
+  std::vector<T> data_; 
+  
+  void swap(size_t pos); 
 };
 
 // Add implementation of member functions here
+template <typename T, typename PComparator>
+Heap<T, PComparator>::Heap(int m=2, PComparator c = PComparator()) {
+  m_ = m; 
+  comp_ = c; 
+}
 
+template <typename T, typename PComparator>
+Heap<T, PComparator>::~Heap() {
+}
+
+template <typename T, typename PComparator>
+void Heap<T, PComparator>::push(const T& item) {
+  size_t item_pos = data_.size(); 
+  data_.push_back(item); 
+
+  while (item_pos > 0 && comp_(data_[item_pos], data_[(item_pos - 1)/m])){
+    std::swap(data_[item_pos], data_[(item_pos - 1)/m]);
+    item_pos = (item_pos - 1)/m; 
+  }
+}
 
 // We will start top() for you to handle the case of 
 // calling top on an empty heap
@@ -81,14 +101,12 @@ T const & Heap<T,PComparator>::top() const
     // ================================
     // throw the appropriate exception
     // ================================
-
+    throw std::underflow_error("Heap is empty"); 
 
   }
   // If we get here we know the heap has at least 1 item
   // Add code to return the top element
-
-
-
+  return data_.front(); 
 }
 
 
@@ -101,11 +119,44 @@ void Heap<T,PComparator>::pop()
     // ================================
     // throw the appropriate exception
     // ================================
-
-
+    throw std::underflow_error("Heap is empty"); 
   }
 
+  if(data_.size() == 1){
+    data_.pop_back(); 
+    return; 
+  }
 
+  std::swap(data_[0], data_[data_.size() - 1]);
+  data_.pop_back();
+
+  size_t item_pos = 0; 
+  size_t bestChild = 1; 
+
+  while(true){
+    if (item_pos * m_ + 1 >= data_.size()){ // no children
+      break; 
+    }
+
+    size_t max_bound = data_.size() - (item_pos * m_ + m_); 
+    if (max_bound > m_){ // checks how many children. 
+      max_bound = m_; 
+    }
+
+    for(int i = 1; i < max_bound; i++){
+      if(! comp_(data_[item_pos*m_ + bestChild], data_[item_pos*m_ + i])){
+        bestChild = i; 
+      }
+    }
+
+    if(item_pos < data_.size() && comp_(data_[item_pos * m_ + bestChild], data_[item_pos])){
+      std::swap(data_[item_pos], data_[item_pos * m_ + bestChild]); 
+      item_pos = item_pos * m_ + bestChild; 
+    }
+    else {
+      break; 
+    }
+  }
 
 }
 
